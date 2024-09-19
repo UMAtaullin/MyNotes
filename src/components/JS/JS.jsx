4bfcd02fc3d7cb41a2581780622d8308c55fe98f
UMAtaullin <ataullinural@gmail.com>
2024-09-19 16:05:24 +0500

    navigation optimization: replacing div with semantic list items

--- src/components/JS/JS.jsx
+++ src/components/JS/JS.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
-import Variables from '../Topics/Variables'
-import Functions from '../Topics/Function'
-import Objects from '../Topics/Objects'
+import Variables from './Topics/Variables'
+import Functions from './Topics/Function'
+import Objects from './Topics/Objects'
 import cls from './JS.module.css'
 
 const JS = () => {
   return (
     <div className={cls.js}>
       <nav className={cls.items}>
-        <div className={cls.item}>
-          <NavLink to='/js/variables/'>Переменные</NavLink>
-        </div>
-        <div className={cls.item}>
-          <NavLink to='/js/functions/'>Функции</NavLink>
-        </div>
-        <div className={cls.item}>
-          <NavLink to='/js/objects/'>Объекты</NavLink>
-        </div>
+        <ul>
+          <li className={cls.item}>
+            <NavLink to='/js/variables/'>Переменные</NavLink>
+          </li>
+          <li className={cls.item}>
+            <NavLink to='/js/functions/'>Функции</NavLink>
+          </li>
+          <li className={cls.item}>
+            <NavLink to='/js/objects/'>Объекты</NavLink>
+          </li>
+        </ul>
       </nav>
-        
+
       <div className={cls.cont}>
         <Routes>
-          <Route path='/variables/' element={<Variables/>} />
-          <Route path='/functions/' element={<Functions/>} />
-          <Route path='/objects/' element={<Objects/>} />
+          <Route path='/variables/' element={<Variables />} />
+          <Route path='/functions/' element={<Functions />} />
+          <Route path='/objects/' element={<Objects />} />
         </Routes>
       </div>
     </div>
